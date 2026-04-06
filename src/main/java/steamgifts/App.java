7a2e17672632f4046b89afe0f4d738b0b4625a94
zap4ick <zap4ick@gmail.com>
2026-04-06 13:51:54 +0200

    workarounding captcha

--- src/main/java/steamgifts/App.java
+++ src/main/java/steamgifts/App.java
@@ -53,6 +53,8 @@ public class App {
 
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-blink-features=AutomationControlled");
+        options.addArguments("--headless=new");
+        options.addArguments("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36");
         options.setExperimentalOption("excludeSwitches", new String[]{"enable-automation"});
         options.setExperimentalOption("useAutomationExtension", false);
         Configuration.browserCapabilities = options;
