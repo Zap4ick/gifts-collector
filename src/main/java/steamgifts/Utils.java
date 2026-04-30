985c459c3b6e45f32351552f62839bd022fd5872
zap4ick <zap4ick@gmail.com>
2026-04-30 23:58:09 +0200

    user agent updated

--- src/main/java/steamgifts/Utils.java
+++ src/main/java/steamgifts/Utils.java
@@ -28,7 +28,6 @@ public class Utils {
     public static ChromeOptions buildStealthOptions() {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-blink-features=AutomationControlled");
-        options.addArguments("--headless=new");
         options.addArguments("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/147.0.7727.56 Safari/537.36");
         options.addArguments("--lang=en-US,en;q=0.9");
         options.addArguments("--window-size=1366,768");
